cc45564ef32f7fe44d2b2c2a8bb4b999b0ee3c52
Ali Rasouli Fard <ali.rasoulifard.edu@gmail.com>
2026-04-06 19:52:17 +0200

    create display

--- game.py
+++ game.py
@@ -2,3 +2,13 @@ import pygame
 import sys
 
 pygame.init()
+game_display = pygame.display.set_mode((1000, 600))
+pygame.display.set_caption('Dino Game')
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        pygame.display.update()
