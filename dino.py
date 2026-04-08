e2afdd207130180b15f56065d40cbadc4a170714
Ali Rasouli Fard <ali.rasoulifard.edu@gmail.com>
2026-04-09 01:17:42 +0200

    add background in display

--- dino.py
+++ dino.py
@@ -11,27 +11,50 @@ class Dino():
     def __init__(self, location,fps):
         self.location = location
         self.x_loc, self.y_loc = location
+        self.Y_LOC = self.y_loc
         self.fps = fps
         self.walk_img = True
         self.walk_slicer = 8
         self.walk_counter = 0
+        self.state = True
+        self.jump_threshold = self.y_loc - 200
+        self.YD = 5
+        self.yd = self.YD
+        self.rect = self.img_walk1.get_rect(topleft = self.location)
 
     def update(self, surface):
-        if self.walk_counter < self.fps:
-            if self.walk_counter % self.walk_slicer == 0:
-                self.walk_img = not self.walk_img
-            self.walk_counter += 1
+        if self.state:
+            if self.walk_counter < self.fps:
+                if self.walk_counter % self.walk_slicer == 0:
+                    self.walk_img = not self.walk_img
+                self.walk_counter += 1
+
+            else:
+                self.walk_counter = 0
+
+            if self.walk_img:
+                surface.blit(self.img_walk1, self.location)
+            else:
+                surface.blit(self.img_walk2, self.location)
 
         else:
-            self.walk_counter = 0
+            if self.yd > 0: # going up
+                if self.y_loc < self.jump_threshold:
+                    self.yd *= -1
 
-        if self.walk_img:
-            surface.blit(self.img_walk1, self.location)
-        else:
-            surface.blit(self.img_walk2, self.location)
+            else: # going down
+                if self.y_loc > self.Y_LOC:
+                    self.state = True
+                    self.yd = self.YD
+            
+            self.y_loc -= self.yd
+            surface.blit(self.img_jump, (self.x_loc, self.y_loc))
+            self.rect.topleft = (self.x_loc, self.y_loc)
+
 
         
-        
+    def jump(self):
+        self.state = False
 
-    
+
 
--- game.py
+++ game.py
@@ -9,11 +9,15 @@ class Game:
         self.game_display = pygame.display.set_mode((1000, 600))
         pygame.display.set_caption('Dino Game')
 
-        self.player1 = Dino((500,300), self.fps)
+        self.player1 = Dino((100,390), self.fps) # Dino location in display
         self.clock = pygame.time.Clock()
         self.run()
 
     def run(self):
+
+        rect1 = pygame.Rect(800, 150, 50, 50)
+        ground = pygame.image.load("./assets/desert.png")
+
         while True:
             self.game_display.fill((0,0,0))
             for event in pygame.event.get():
@@ -21,6 +25,18 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.player1.jump()
+
+            rect1.x -= 1
+            pygame.draw.rect(self.game_display, (255,255,255), rect1)
+            if rect1.colliderect(self.player1.rect):
+                print("Collision")
+            else:
+                print("No Collision")
+            
+            self.game_display.blit(ground, (0, 450)) # Ground location in display
             self.player1.update(self.game_display)
 
             #
