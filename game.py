3bf9eb83109f2a25cffb90279228cb8917177878
Ali Rasouli Fard <ali.rasoulifard.edu@gmail.com>
2026-04-08 16:17:28 +0200

    create display with class

--- game.py
+++ game.py
@@ -1,14 +1,23 @@
 import pygame
 import sys
 
-pygame.init()
-game_display = pygame.display.set_mode((1000, 600))
-pygame.display.set_caption('Dino Game')
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.game_display = pygame.display.set_mode((1000, 600))
+        pygame.display.set_caption('Dino Game')
+
+        self.run()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                pygame.display.update()
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        pygame.display.update()
+ 
+
