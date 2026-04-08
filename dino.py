066864fcd72188556d164a69da29d492f8a18cb3
Ali Rasouli Fard <ali.rasoulifard.edu@gmail.com>
2026-04-08 17:11:42 +0200

    created animation. V01

--- dino.py
+++ dino.py
@@ -7,11 +7,31 @@ class Dino():
     img_walk1 = pygame.image.load(prefix + "trex_run1.png")
     img_walk2 = pygame.image.load(prefix + "trex_run2.png")
     img_jump = pygame.image.load(prefix + "trex_jump.png")
-    
-    def __init__(self, location):
+
+    def __init__(self, location,fps):
         self.location = location
         self.x_loc, self.y_loc = location
-
+        self.fps = fps
+        self.walk_img = True
+        self.walk_slicer = 8
+        self.walk_counter = 0
 
     def update(self, surface):
-        surface.blit(self.img_walk1, self.location)
+        if self.walk_counter < self.fps:
+            if self.walk_counter % self.walk_slicer == 0:
+                self.walk_img = not self.walk_img
+            self.walk_counter += 1
+
+        else:
+            self.walk_counter = 0
+
+        if self.walk_img:
+            surface.blit(self.img_walk1, self.location)
+        else:
+            surface.blit(self.img_walk2, self.location)
+
+        
+        
+
+    
+
--- game.py
+++ game.py
@@ -3,24 +3,30 @@ import sys
 from dino import Dino
 
 class Game:
+    fps = 60
     def __init__(self):
         pygame.init()
         self.game_display = pygame.display.set_mode((1000, 600))
         pygame.display.set_caption('Dino Game')
 
-        self.player1 = Dino((500,300))
-
+        self.player1 = Dino((500,300), self.fps)
+        self.clock = pygame.time.Clock()
         self.run()
 
     def run(self):
         while True:
+            self.game_display.fill((0,0,0))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                self.player1.update(self.game_display)
-                pygame.display.update()
 
+            self.player1.update(self.game_display)
+
+            #
+            pygame.display.update()
+
+            self.clock.tick(self.fps)
 
 new_game = Game()
 
